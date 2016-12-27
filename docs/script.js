27acc561a72b07e8ab126994e4eb8f0954b1345a
Chukwuemeka Aligbe <cealigbe@gmail.com>
2016-12-26 20:47:23 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -47,6 +47,4 @@ submitButton.addEventListener('click', function() {
 	var return_to = getQueryParam('return_to', 'pebblejs://close#');
 
 	document.location = return_to + encodeURIComponent(JSON.stringify(settings));
-
-	window.close();
 });
